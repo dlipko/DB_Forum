e83168886460988b577a2557e5782ce34e8a4ab9
dlipko <lipko_dima@mail.ru>
2019-01-02 15:58:31 +0300

    second voise with id

--- controllers/vote.js
+++ controllers/vote.js
@@ -5,15 +5,26 @@ const threadController = require('./thread');
 class VoteController {
   constructor() {}
 
-  async createVote(nickname, threadSlug, voise) {
-    const sqlQuery = `INSERT INTO votes (nickname, thread_id, voice)
-      VALUES ($1, (SELECT id FROM threads WHERE slug = $2), $3);`;
+  async createVote(nickname, threadIdOrSlug, voise) {
+      let sqlQuery;
+      const parse = parseInt(threadIdOrSlug);
+    if (parse) {
+        threadIdOrSlug = parse;
+        sqlQuery = `INSERT INTO votes (nickname, thread_id, voice) VALUES ($1, $2, $3);`;
     //   ON CONFLICT DO UPDATE SET voice = $3 WHERE votes.thread_id = (SELECT id FROM threads WHERE slug = $2) AND votes.nickname = $1;`;
-    const answer = await query(sqlQuery, [nickname, threadSlug, voise]);
-    console.log('PARAMS', [nickname, threadSlug, voise]);
-    const thread = await threadController.findThreadBySlug(threadSlug);
+    } else {
+        sqlQuery = `INSERT INTO votes (nickname, thread_id, voice)
+            VALUES ($1, (SELECT id FROM threads WHERE slug = $2), $3);`;
+    //   ON CONFLICT DO UPDATE SET voice = $3 WHERE votes.thread_id = (SELECT id FROM threads WHERE slug = $2) AND votes.nickname = $1;`;
+    }
+    
+    
+    const answer = await query(sqlQuery, [nickname, threadIdOrSlug, voise]);
+    console.log('PARAMS', [nickname, threadIdOrSlug, voise]);
+    const thread = /^[0-9]*$/i.test(threadIdOrSlug) ? await threadController.findThreadById(threadIdOrSlug) :
+  await threadController.findThreadBySlug(threadIdOrSlug);
 
-    if (answer.rowCount != 0) {
+    if (thread) {
         return thread;
       }
       else {
