6fcd9586eec90967caf7ccf4d6042c9d24238920
dlipko <lipko_dima@mail.ru>
2019-01-08 00:44:33 +0300

    more bad fixes

--- controllers/vote.js
+++ controllers/vote.js
@@ -6,19 +6,21 @@ class VoteController {
   constructor() {}
 
   async createVote(nickname, threadIdOrSlug, voise) {
-      let sqlQuery;
+      let sqlQuery = `INSERT 
+      INTO votes (nickname, thread, voice) 
+      VALUES ('${nickname}', `;
+
     if (/^[0-9]*$/i.test(threadIdOrSlug)) {
         threadIdOrSlug = parseInt(threadIdOrSlug, 10);
-        sqlQuery = `INSERT INTO votes (nickname, thread, voice) VALUES ($1, $2, $3)
-        ON CONFLICT (nickname, thread) DO UPDATE SET voice = $3;`;
+        sqlQuery += ` ${threadIdOrSlug}`;
     } else {
-        sqlQuery = `INSERT INTO votes (nickname, thread, voice)
-            VALUES ($1, (SELECT id FROM threads WHERE slug = $2), $3)
-            ON CONFLICT (nickname, thread) DO UPDATE SET voice = $3;`;
+        sqlQuery += ` (SELECT id FROM threads WHERE slug = '${threadIdOrSlug}') `;
     }
 
+    sqlQuery += `, ${voise})  ON CONFLICT (nickname, thread) DO UPDATE SET voice = ${voise};`;
+
     // console.log(sqlQuery);
-    const answer = await query(sqlQuery, [nickname, threadIdOrSlug, voise]);
+    const answer = await query(sqlQuery, []);
 
     // console.log(answer);
 
